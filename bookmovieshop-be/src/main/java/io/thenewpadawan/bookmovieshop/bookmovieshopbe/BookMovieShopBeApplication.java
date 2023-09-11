49df967aaa30e95c2b186d7d1bf7970ed34eb0c6
TheNewPadawan <lambertofulgione1@gmail.com>
2023-09-11 19:37:11 +0200

    BE implemented

---
+++ bookmovieshop-be/src/main/java/io/thenewpadawan/bookmovieshop/bookmovieshopbe/BookMovieShopBeApplication.java
@@ -0,0 +1,13 @@
+package io.thenewpadawan.bookmovieshop.bookmovieshopbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMovieShopBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMovieShopBeApplication.class, args);
+	}
+
+}
---
+++ bookmovieshop-be/src/test/java/io/thenewpadawan/bookmovieshop/bookmovieshopbe/BookMovieShopBeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.thenewpadawan.bookmovieshop.bookmovieshopbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMovieShopBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
