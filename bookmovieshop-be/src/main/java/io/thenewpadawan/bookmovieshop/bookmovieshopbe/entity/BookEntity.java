4aa88f64a2bac2c231a7e523f64fa0cb4575dddf
TheNewPadawan <lambertofulgione1@gmail.com>
2023-09-22 19:15:14 +0200

    Now entities match db

--- bookmovieshop-be/src/main/java/io/thenewpadawan/bookmovieshop/bookmovieshopbe/entity/BookEntity.java
+++ bookmovieshop-be/src/main/java/io/thenewpadawan/bookmovieshop/bookmovieshopbe/entity/BookEntity.java
@@ -1,11 +1,19 @@
 package io.thenewpadawan.bookmovieshop.bookmovieshopbe.entity;
 
-import java.util.List;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
+@Entity
+@Table(name = "book", schema = "book_movie_shop")
 @Data
 public class BookEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	private String title;
 	private Integer firstEditionYear;
 	private Integer editionNumber;
--- bookmovieshop-be/src/main/java/io/thenewpadawan/bookmovieshop/bookmovieshopbe/entity/MovieEntity.java
+++ bookmovieshop-be/src/main/java/io/thenewpadawan/bookmovieshop/bookmovieshopbe/entity/MovieEntity.java
@@ -1,12 +1,25 @@
 package io.thenewpadawan.bookmovieshop.bookmovieshopbe.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
+@Entity
+@Table(name = "movie", schema = "book_movie_shop")
 @Data
 public class MovieEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	private String title;
 //	@ManyToOne
 	private String director;
 	private Integer year;
 	private String genre;
 }
+//select * from movie where title = "oddisea"
+//in hibernate:
+//select m from MovieEntity m where m.title = "oddisea"
